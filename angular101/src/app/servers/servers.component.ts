319b865c30aeba0c490595f1c9becd9862eab744
Smolmeri <paavolainent@gmail.com>
2019-08-20 15:17:28 +0300

    directives task

--- angular101/src/app/servers/servers.component.ts
+++ angular101/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = "No server was created!";
   serverName: any;
   userName = "";
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 
