c88d5443adb62c23682932e0e16363d04544d09e
Smolmeri <paavolainent@gmail.com>
2019-08-20 12:13:41 +0300

    Task1, created 2 new components and styles etc

---
+++ angular101/src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning',
+    templateUrl: './warningAlert.component.html',
+    styleUrls: ['./warningAlert.component.css']
+})
+
+export class WarningAlertComponent {
+
+}
