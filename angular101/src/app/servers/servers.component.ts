17e652255e420eb28d1cfb09e22afb9c6d4deb37
Smolmeri <paavolainent@gmail.com>
2019-08-20 13:55:42 +0300

    two-way databinding

--- angular101/src/app/servers/servers.component.ts
+++ angular101/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
+  serverName: any;
 
   constructor() {
     setTimeout(() => {
@@ -20,7 +21,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created";
+    this.serverCreationStatus = "Server was created! Name is "+this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 
 }
