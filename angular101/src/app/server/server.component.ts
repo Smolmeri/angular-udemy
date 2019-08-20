55303941634a4d27353b7e0fee2ca418272c2916
Smolmeri <paavolainent@gmail.com>
2019-08-20 13:28:29 +0300

    string interpolation

--- angular101/src/app/server/server.component.ts
+++ angular101/src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    serverId  = 10;
+    serverStatus = 'online';
 
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
