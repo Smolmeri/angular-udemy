a53b1b306db07dfef4907e2195ddca880c06197c
Smolmeri <paavolainent@gmail.com>
2019-08-23 11:00:53 +0300

    setup navigation between pages

--- shopping-list/src/app/header/header.component.ts
+++ shopping-list/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
     selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+   @Output() featureSelected = new EventEmitter<string>();
 
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
